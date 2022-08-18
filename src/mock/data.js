1fb1be5513df8580ff27cfe409b13ed160e41d9e
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-08-18 18:06:34 +0900

    制作期間を追加

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,7 +30,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project4.jpg',
     title: 'Apple Music Hi-Res Albums Database',
-    info: '【使用技術】',
+    info: '制作時期：2021/10/18〜2022/2/5',
     info2: ['React(Next.js)', 'Firebase', 'React-Bootstrap', 'TypeScript'],
     url: 'https://www.applemusichirezdb.com',
     repo: 'https://github.com/totuus1157/apple-music-hi-res-album-database', // if no repo, the button will not show up
@@ -39,7 +39,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project3.jpg',
     title: 'メモのできるカレンダー',
-    info: '【使用技術】',
+    info: '制作時期：2021/7/5〜2021/9/12',
     info2: ['React(Next.js)', 'Firebase', 'React-Bootstrap', 'Docker Compose', 'TypeScript'],
     url: 'http://next-calendar-app.vercel.app/',
     repo: 'https://github.com/totuus1157/next-calendar-app', // if no repo, the button will not show up
@@ -48,7 +48,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project2.jpg',
     title: '20世紀シンセサイザーギャラリー',
-    info: '【使用技術】',
+    info: '制作時期：2021/3/2〜2021/3/23',
     info2: ['React(Create React App)', 'Material UI', 'Docker Compose', 'TypeScript'],
     url: 'https://totuus1157.github.io/20th-synthesizers-gallery/',
     repo: 'https://github.com/totuus1157/20th-synthesizers-gallery', // if no repo, the button will not show up
@@ -57,7 +57,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project1.jpg',
     title: '年賀状当選番号チェッカー',
-    info: '【使用技術】',
+    info: '制作時期：2021/1/18〜2021/1/29',
     info2: ['React(Next.js)'],
     url: 'https://new-years-card-winner-number-checker.vercel.app',
     repo: 'https://github.com/totuus1157/new-years-card-winner-number-checker', // if no repo, the button will not show up
